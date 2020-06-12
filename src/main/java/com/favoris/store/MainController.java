eb2ce59f1fb773236d5d7294eab995237af6fe3a
vova99 <mr.oleksiyenko@gmail.com>
2020-06-12 21:06:41 +0300

    dslfs

--- src/main/java/com/favoris/store/MainController.java
+++ src/main/java/com/favoris/store/MainController.java
@@ -2,6 +2,7 @@ package com.favoris.store;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by uer on 12.06.20.
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class MainController {
     @RequestMapping("/")
+    @ResponseBody
     String index(){
         return "Work";
     }
